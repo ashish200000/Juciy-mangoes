c705f2c8e0fff4388f277bb3ac4fc8a8d57331ec
ashish200000 <78677208+ashish200000@users.noreply.github.com>
2021-10-12 13:24:54 +0530

    Add files via upload

---
+++ C-28/Project-C28-template-main/launcher.js
@@ -0,0 +1,22 @@
+class launcher{
+   constructor(bodyA,pointB){
+       var  options = {
+           bodyA:bodyA,
+           pointB:pointB,
+           stiffness:0.01,
+           length:10
+       }
+       this.pointB=pointB
+       this.launcher = Constraint.create(options)
+       World.add(world,this.launcher)
+   }
+   
+    fly(){this.launcher.bodyA=null}
+    
+    Launch(bodyA){
+        this.bodyA.launcher.bodyA = bodyA
+    }
+    display(){
+        
+    }
+}
